22e0fb4dddbb877f73f83b6442f2d540ad139ad3
Steven <sastudillo6@gmail.com>
2023-01-18 02:39:22 -0400

    Basic boilerplate for contact email.

--- src/components/ContactPage.js
+++ src/components/ContactPage.js
@@ -1,10 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 const ContactPage = () => {
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        message: ''
+    });
+
+    const handleChange = (event) => {
+        setFormData({
+            ...formData,
+            [event.target.name]: event.target.value
+        });
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(formData);
+        // placeholder code to add email serverrrr.
+    }
+
     return (
         <div className='contact-page'>
-            <h1>ContactPage Component</h1>
+            <h1>Contact Me</h1>
+            <form onSubmit={handleSubmit}>
+                <label>
+                    Name:
+                    <input type="text" name="What's your name?" value={formData.name} onChange={handleChange} required />
+                </label>
+                <label>
+                    Email:
+                    <input type="Where can I reach you?" name="email" value={formData.email} onChange={handleChange} required />
+                </label>
+                <label>
+                    Message:
+                    <textarea name="What's up?" value={formData.message} onChange={handleChange} required></textarea>
+                </label>
+                <button type="submit">Send</button>
+            </form>
+            <Link to='/'>Go back home</Link>
         </div>
     );
 };
